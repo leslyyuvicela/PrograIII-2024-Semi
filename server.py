54f16d26c5808e852b6b3b07b955e0a8cdf90066
leslyyuvicela <yuvicelaguzman14727@gmail.com>
2024-10-16 23:57:24 -0600

    Base de datos exportada

--- server.py
+++ server.py
@@ -5,10 +5,12 @@ import json
 import crud_alumno
 import crud_docente
 import crud_materia
+import crud_usuario
 
 crudAlumno = crud_alumno.crud_alumno()
 crudDocente = crud_docente.crud_docente()
 crudMateria = crud_materia.crud_materia()
+crudUsuario = crud_usuario.crud_usuario()
 class servidorBasico(SimpleHTTPRequestHandler):
     def do_GET(self):
         url_parseada = urlparse(self.path)
@@ -27,6 +29,7 @@ class servidorBasico(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(json.dumps(crudAlumno.consultar()).encode('utf-8'))
+
         elif path == '/docentes':
             self.send_response(200)
             self.end_headers()
@@ -36,26 +39,45 @@ class servidorBasico(SimpleHTTPRequestHandler):
             self.send_response(200)
             self.end_headers()
             self.wfile.write(json.dumps(crudMateria.consultar()).encode('utf-8'))
+
+        elif path == '/usuarios':
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(json.dumps(crudUsuario.consultar()).encode('utf-8'))
+    
     def do_POST(self):
+       try:
         longitud = int(self.headers['Content-Length'])
         body = self.rfile.read(longitud)
         datos = body.decode('utf-8')
         datos = parse.unquote(datos)
         datos = json.loads(datos)
-        resp = {"msg": crudAlumno.administrar(datos)}
-        
-        if self.path == '/alumnos':   
+
+        if self.path == '/alumnos':
             resp = {"msg": crudAlumno.administrar(datos)}
-            
+
         elif self.path == '/docentes':
             resp = {"msg": crudDocente.administrar(datos)}
 
         elif self.path == '/materias':
             resp = {"msg": crudMateria.administrar(datos)}
+
+        elif self.path == '/usuarios':
+            resp = {"msg": crudUsuario.administrar(datos)}
+        
+        else:
+            self.send_error(404, "Ruta no encontrada")
+            return
             
         self.send_response(200)
         self.end_headers()
         self.wfile.write(json.dumps(resp).encode('utf-8'))
 
+       except Exception as e:
+        self.send_error(500, f"Error al procesar la solicitud: {str(e)}")
+
+
+
 server = HTTPServer(('localhost', 3000), servidorBasico)
 print("Servidor ejecutado en el puerto 3000")
+server.serve_forever()
