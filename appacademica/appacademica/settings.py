837a501ab9821b770c61c09bb29aec7a38c6e5e2
leslyyuvicela <yuvicelaguzman14727@gmail.com>
2024-10-23 06:45:38 -0600

    Terminacion del sistema academico con django, docentes, materias

--- appacademica/appacademica/settings.py
+++ appacademica/appacademica/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'academica'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'appacademica.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'academica/plantillas'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
